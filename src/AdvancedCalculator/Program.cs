89de4a335a68a0c66b41db3e18539ba752910e82
Azaliya Muhamethanova <kasaevaaza906@gmail.com>
2025-12-01 22:26:20 +0300

    feat: Изменение

--- src/AdvancedCalculator/Program.cs
+++ src/AdvancedCalculator/Program.cs
@@ -13,7 +13,7 @@ class Program
         Console.WriteLine($"2^4 = {calculator.Power(2, 4)}");
         Console.WriteLine($"5! = {calculator.Factorial(5)}");
         Console.WriteLine($"17 простое число? {calculator.IsPrime(17)}");
-        Console.WriteLine($"Среднее [1,2,3,4,5] = {stats.CalculateAverage(1,2,3,4,5)}");
+        Console.WriteLine($"Среднее [1, 2, 3, 4, 5] = {stats.CalculateAverage(1, 2, 3, 4, 5)}");
 
         Console.WriteLine("\nВсе операции выполнены успешно!");
     }
